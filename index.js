c74e8aa5f8a68e3462b442f956085a6a06fbbe4c
Akshat Sharma <akshatsharma8304@gmail.com>
2023-09-28 22:58:33 +0530

    trying a new design

--- index.js
+++ index.js
@@ -1,56 +1,99 @@
-const { getDepOptimizationConfig, formatPostcssSourceMap } = require("vite");
+// const { getDepOptimizationConfig, formatPostcssSourceMap } = require("vite");
 
-const userTab = document.querySelector("[data-userWeather]");
-const searchTab = document.querySelector("[data-searchWeather]");
-const userContainer = document.querySelector(".weather-container");
-const grantAccessContainer = document.querySelector(".grant-location-container");
-const searchForm = document.querySelector("[data=searchForm");
-const loadingScreen = document.querySelector(".loading-container");
-const userInfoContainer = document.querySelector(".user-info-container");
-
-
-let oldTab = userTab;
-const API_KEY = "deaa72d41fd80130a76aebfa79ba2dd7";
-
-currentTab.classList.add("current-tab");
+// const userTab = document.querySelector("[data-userWeather]");
+// const searchTab = document.querySelector("[data-searchWeather]");
+// const userContainer = document.querySelector(".weather-container");
+// const grantAccessContainer = document.querySelector(".grant-location-container");
+// const searchForm = document.querySelector("[data=searchForm");
+// const loadingScreen = document.querySelector(".loading-container");
+// const userInfoContainer = document.querySelector(".user-info-container");
 
 
-// function that switches tabs w.r.t the click event listener
+// let oldTab = userTab;
+// const API_KEY = "deaa72d41fd80130a76aebfa79ba2dd7";
 
-function switchTab(newTab){
-
-    if(newTab != oldTab){
-        oldTab.classList.remove("current-tab");
-        oldTab = newTab;
-        oldTab.classList.add("current-tab");
-    }
-
-    if(!searchForm.classList.contains("active")){
-        userInfoContainer.classList.remove("active");
-        grantAccessContainer.classList.remove("active");
-        searchForm.classList.add("active");
-    }else{
-        searchForm.classList.remove("active");
-        userInfoContainer.classList.remove("active");
-        getFromSessionStorage();
-    }
+// userTab.classList.add("current-tab");
 
 
-}
+// // function that switches tabs w.r.t the click event listener
 
-// change tabs on click
+// function switchTab(newTab){
 
-userTab.addEventListener("click", ()=>{
-    switchTab(userTab);
-});
+//     if(newTab != oldTab){
+//         oldTab.classList.remove("current-tab");
+//         oldTab = newTab;
+//         oldTab.classList.add("current-tab");
+//     }
 
-searchTab.addEventListener("click", ()=>{
-    switchTab(searchTab);
-});
+//     if(!searchForm.classList.contains("active")){
+//         userInfoContainer.classList.remove("active");
+//         grantAccessContainer.classList.remove("active");
+//         searchForm.classList.add("active");
+//     }else{
+//         searchForm.classList.remove("active");
+//         userInfoContainer.classList.remove("active");
+//         getFromSessionStorage();
+//     }
 
 
-// checks if coordinates are already present in session storage
-function getFromSessionStorage(){
+// }
+
+// // change tabs on click
+
+// userTab.addEventListener("click", ()=>{
+//     switchTab(userTab);
+// });
+
+// searchTab.addEventListener("click", ()=>{
+//     switchTab(searchTab);
+// });
+
+
+// // checks if coordinates are already present in session storage
+// function getFromSessionStorage(){
     
+//     const localCoordinates = sessionStorage.getItem("user-coordinates");
+
+//     if(!localCoordinates){
+//         grantAccessContainer.classList.add("active");
+//     }else{
+//         const coordinates = JSON.parse(localCoordinates);
+//         fetchUserWeatherInfo(coordinates);  
+//     }
+
+// }
+
+const day = document.querySelector("[day]");
+const n_date = document.querySelector("[date]");
+const loc = document.querySelector("[location]");
+
+let currentDate = new Date();
+let date = currentDate.getDate();
+let month = currentDate.getMonth()+1;
+let year = currentDate.getFullYear();
+
+console.log(date);
+console.log(month);
+console.log(year);
+
+const API_KEY = 'deaa72d41fd80130a76aebfa79ba2dd7';
+let CITY_NAME = 'goa';
+
+
+getData();
+async function getData(){
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`);
+
+    const data = await response.json();
+    console.log(data);
+    displayData(data);
+}
+
+function displayData(data){
+
+
+
 
 }
+
